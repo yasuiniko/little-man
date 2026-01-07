da243cdcfad5ccedb59cc0f1544552fdd5c6d87c
Niko Yasui <yasuiniko@gmail.com>
2026-01-06 23:52:21 -0700

    add testing docs

--- README.md
+++ README.md
@@ -1,7 +1,14 @@
-```bash
-elm make src/Main.elm --output=main.js && open index.html
-```
+## Compilation
 
+```bash
+elm make src/Main.elm --output=main.js
+```
 Sound requires http, so use a server like the VSCode Live Server extension if you want audio. 
 
-LLMs used throughout for small questions and to write all of Index.html. This was an Elm learning project and I didn't want to do a bunch of js. 
+## Testing
+If you want a user install you can check out the install instructions at the github link below. These instructions are for a global installation.
+```bash
+curl https://sh.rustup.rs -sSf | sh
+cargo install --git https://github.com/mpizenberg/elm-test-rs --tag v3.0.1
+elm-test-rs
+```
--- tests/Tests.elm
+++ tests/Tests.elm
@@ -3,9 +3,8 @@ module Tests exposing (..)
 import Expect
 import Test exposing (..)
 import Dict
-import Domain.Store.Utils exposing (getItemPps) -- Replace with your module name
-import Domain.Store.Model exposing (ItemId(..))
-import Domain.Store.Model exposing (itemMeta)
+import Domain.Store.Utils exposing (getItemPps)
+import Domain.Store.Item exposing (ItemId(..), itemMeta)
 import Domain.Store.Utils exposing (getCursorFactoryBonus)
 import Domain.Store.Utils exposing (getNeighborBoost)
 
@@ -19,7 +18,7 @@ suite =
                     item = { id = Grandma, qty = 5, basePps = 1.0, curPps = 1.0, description = "Grandma", baseCost = 15 }
                     owned = Dict.fromList [ ("Grandma", 5) ]
                     
-                    -- We expect (1.0 + 0.0) * (1.02^5) * (1.005^5) * 1.0
+                    -- We expect (1.0 + 0.0) * (1.02^5-1) * (1.005^5) * 1.0
                     expected = (1.0 + 0.0) * (1.02^4) * (1.005^5) * 1.0
                     result = getItemPps item owned
                 in
@@ -32,7 +31,8 @@ suite =
                     -- Setting up 25 other items to trigger the 0.1 flat boost
                     owned = Dict.fromList [ ("Cursor", 1), ("Factory", 1), ("Grandma", 25) ]
 
-                    expected =  0.1
+                    -- expect 0.01 * 26, since 26 other items
+                    expected =  0.01 * 26
                     result = getCursorFactoryBonus (itemMeta item.id).name owned
                 in
                 result |> Expect.within (Expect.Absolute 0.0001) expected
@@ -41,9 +41,9 @@ suite =
             \_ ->
                 let
                     item = { id = Grandma, qty = 1, basePps = 0.1, curPps = 0.1, baseCost = 10, description = "Grandma"}
-                    owned = Dict.fromList [ ("Farm", 25), ("Grandma", 1) ]
+                    owned = Dict.fromList [ ("Farm", 25.0), ("Grandma", 1) ]
 
-                    expected = 1.3^3
+                    expected = 1.03^owned['Farm']
                     result = getNeighborBoost (itemMeta item.id).name owned
                 in
                 result |> Expect.within (Expect.Absolute 0.0001) expected
@@ -55,7 +55,7 @@ suite =
                     -- Setting up 25 other items to trigger the 0.1 flat boost
                     owned = Dict.fromList [ ("Cursor", 1), ("Factory", 1), ("Grandma", 25) ]
 
-                    expected =  (0.1 + 0.1) * (1.02^0) * (1.005^25) * 1.0
+                    expected =  (0.1 + 0.01*26) * (1.02^0) * (1.005^25) * 1.0
                     result = getItemPps item owned
                 in
                 result |> Expect.within (Expect.Absolute 0.0001) expected
